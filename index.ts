8c039c39c6887204408e6156173a6f2f527f7a50
Hassancoder24 <165388429+Hassancoder24@users.noreply.github.com>
2024-05-02 00:59:37 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+//word counter 
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//welcom msg
+console.log(chalk.bgBlueBright("\t\nWellcome to my Word Counter Project\n"));
+
+
+//functions
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message:"\t\n Please Enter your Sentence to count the Sentence words:\n" 
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+//print a word in arrey
+console.log(words);
+//print a number count of words
+console.log(chalk.yellowBright(`\t\nYour Secentence word count is ${words.length}\n`))
